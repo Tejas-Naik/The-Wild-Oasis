cded96a7ad2a5059a260464ffb32b2c278f6d5d5
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-08-07 06:49:09 +0000

    Introduction to Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { styled } from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border:none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: .8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <StyledApp>
+      <H1>Hello World</H1>
+      <Button onClick={()=>alert("Check In")}>Check In</Button>
+      <Button onClick={()=>alert("Check Out")}>Check Out</Button>
+      <Input type="number" placeholder="Number of guests" />
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
+  )
 }
 
-export default App
+export default App;
