956dc1e01bfb2f1c8a16f59e6edcb02de4342ef6
Tejas Naik <RNTejas2005@gmail.com>
2023-08-21 11:51:13 +0530

    Client Side Filtering: Filtering Cabins

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -4,7 +4,11 @@ import Filter from "../../ui/Filter";
 function CabinTableOperations() {
   return (
     <TableOperations>
-      <Filter />
+      <Filter filterField="discount" options={[
+        { value: 'all', label: "All" },
+        { value: 'no-discount', label: "No Discount" },
+        { value: 'with-discount', label: "With discount" },
+      ]} />
     </TableOperations>
   );
 }
