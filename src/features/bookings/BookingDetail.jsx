78af2f6e83f84368236d3d17d5cf9e483ffc1f6b
Tejas Naik <RNTejas2005@gmail.com>
2023-09-03 16:49:46 +0530

    Fixing small bug in menus and showing not found for the booking that doesnt exist

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Button from "../../ui/Button";
 import ButtonGroup from "../../ui/ButtonGroup";
 import ButtonText from "../../ui/ButtonText";
+import Empty from "../../ui/Empty";
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
 import Spinner from "../../ui/Spinner";
@@ -33,7 +34,7 @@ function BookingDetail() {
   const navigate = useNavigate();
 
   if (isLoading || isDeleting) return <Spinner />;
-
+  if (!booking) return <Empty resourceName="booking" />
   const { status, id: bookingId } = booking;
 
   const statusToTagName = {
