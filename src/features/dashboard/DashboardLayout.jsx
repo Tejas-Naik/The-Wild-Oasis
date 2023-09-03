8e22ae090a63d885538d86ea794e3df15772b5eb
Tejas Naik <RNTejas2005@gmail.com>
2023-09-03 14:34:53 +0530

    Displaying Todays activities and checking in and out the guests

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import { useCabins } from "../cabins/useCabins";
+import TodayActivity from "../check-in-out/TodayActivity";
+import DurationChart from "./DurationChart";
 import SalesChart from "./SalesChart";
 import Stats from "./Stats";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
-import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -28,7 +29,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today's activity</div>
+
+      <TodayActivity />
 
       <DurationChart confirmedStays={confirmedStays} />
 
